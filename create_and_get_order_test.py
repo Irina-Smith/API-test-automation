e51cf4051480a25e732949ad240a32aca766441a
Irina-Smith <87424746+Irina-Smith@users.noreply.github.com>
2024-06-14 03:15:58 +0400

    add new files

--- create_and_get_order_test.py
+++ create_and_get_order_test.py
@@ -6,3 +6,5 @@ def test_create_and_get_order():
     number_track = response.json()["track"]
     order_response = sender_stand_request.get_order(number_track)
     assert order_response.status_code == 200
+
+# Ключикова Ирина, 17-я когорта — Финальный проект. Инженер по тестированию плюс
